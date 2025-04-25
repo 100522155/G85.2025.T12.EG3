a87dd7802d20848c4801f09463a8fea6e0a983e8
100522165 <100522165@alumnos.uc3m.es>
2025-04-25 22:20:07 +0200

    crear transfers_json_store

---
+++ src/main/python/uc3m_money/storage/transfers_json_store.py
@@ -0,0 +1,43 @@
+"""transfers_json_store"""
+from uc3m_money import AccountManagementException
+from uc3m_money.storage.json_store import JsonStore
+from uc3m_money.config.account_management_config import TRANSFERS_STORE_FILE
+
+
+class TransfersJsonStore(JsonStore):
+    _file_name = TRANSFERS_STORE_FILE
+
+    def add_item(self, item):
+        """add item"""
+        for transfer in self._data_list:
+            if (transfer == item.to_json()):
+                raise AccountManagementException("Duplicated transfer in transfer list")
+        super().add_item(item)
+
+
+"""
+    class __TransferJsonStore(JsonStore):
+        _file_name =TRANSFERS_STORE_FILE
+
+        def add_item(self,item):
+            self.load_list_from_file()
+            for old_transfer in self._data_list:
+                if old_transfer == item.to_json():
+                    raise AccountManagementException("Duplicated transfer in transfer list")
+                super().add_item(item)
+    instance = None
+
+    def __new__(cls):
+        if not TransferJsonStore.instance:
+            TransferJsonStore.instance = TransferJsonStore.__TransferJsonStore()
+        return TransferJsonStore.instance
+
+
+    def __getattr__(self, item):
+        return getattr(TransferJsonStore.instance, item)
+
+    def __setattr__(self, key, value):
+        return setattr(TransferJsonStore.instance, key, value)
+
+
+"""
